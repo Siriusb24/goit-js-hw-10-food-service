5850cba6d4c57484e28494e8c47dded0cfe4d263
Siriusb24 <Siriusb2324@gmail.com>
2021-08-29 23:42:16 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import './css/styles.scss'
 import foodGallery from './menu.json'
 import foodTamplate from './tamplates/food-gallery.hbs'
+import theme from './js/Theme.js'
 
 // ----------------------------------------------------------------------------
 // ------------------------------- ВСЕ ДОСТУПЫ --------------------------------
@@ -15,28 +16,32 @@ const foodMarkup = foodTamplate(foodGallery)
 foodList.insertAdjacentHTML('beforeend', foodMarkup)
 const lightControl = document.getElementById("theme-switch-toggle")
 
-
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
 
 
 // ----------------------------------------------------------------------------
 // ------------------------------- ВСЕ ФУНКЦИИ --------------------------------
 // ----------------------------------------------------------------------------
-
-//
-
+    function onTargetImput(e){
+            if(e.target.checked === true){
+            body.classList.add(Theme.DARK);
+            localStorage.setItem("class", "dark")
+            } else {
+           body.classList.add(Theme.LIGHT)
+           body.classList.remove(Theme.DARK)
+           localStorage.removeItem("class", "dark")}
+    }
 // ----------------------------------------------------------------------------
 // ------------------------------ ВСЕ СЛУШАТЕЛИ -------------------------------
 // ----------------------------------------------------------------------------
+lightControl.addEventListener('change', onTargetImput)
+   
+if(localStorage.getItem('class')){
+    lightControl.checked = true
+    body.classList.add(Theme.DARK);
+}
 
-lightControl.addEventListener('click', (e) => {
-    console.log(e)
-    if(e.target.checked === true){
-    body.classList.add('dark-theme');
-    localStorage.setItem("class", "dark")
-    } else {
-   body.classList.add('light-theme')
-   body.classList.remove('dark-theme')}
-   let className = localStorage.getItem('class');
-   body.classList.add(className);
-})
-
+   

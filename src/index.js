f89888fd4545557b6c4579e4c5ee7548587dc6e5
Siriusb24 <Siriusb2324@gmail.com>
2021-08-29 22:11:09 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ import './css/styles.scss'
 import foodGallery from './menu.json'
 import foodTamplate from './tamplates/food-gallery.hbs'
 
-
 // ----------------------------------------------------------------------------
 // ------------------------------- ВСЕ ДОСТУПЫ --------------------------------
 // ----------------------------------------------------------------------------
+const body = document.querySelector('body')
 const foodList = document.querySelector(".js-menu")
-console.log(foodList)
+const foodMarkup = foodTamplate(foodGallery)
+foodList.insertAdjacentHTML('beforeend', foodMarkup)
+const lightControl = document.getElementById("theme-switch-toggle")
+
+
+
 
 // ----------------------------------------------------------------------------
 // ------------------------------- ВСЕ ФУНКЦИИ --------------------------------
@@ -23,4 +28,15 @@ console.log(foodList)
 // ------------------------------ ВСЕ СЛУШАТЕЛИ -------------------------------
 // ----------------------------------------------------------------------------
 
-//
+lightControl.addEventListener('click', (e) => {
+    console.log(e)
+    if(e.target.checked === true){
+    body.classList.add('dark-theme');
+    localStorage.setItem("class", "dark")
+    } else {
+   body.classList.add('light-theme')
+   body.classList.remove('dark-theme')}
+   let className = localStorage.getItem('class');
+   body.classList.add(className);
+})
+
